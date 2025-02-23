99b62c7d123fc3d01e620be2d01b1eeb5b6d51c4
Elias Bennett <eliasthebennett@gmail.com>
2025-02-23 00:52:45 +0000

    Adding a hamburger menu to the nav on mobile.

--- script.js
+++ script.js
@@ -89,3 +89,21 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
+// Hamburger menu
+document.addEventListener("DOMContentLoaded", () => {
+    const menuIcon = document.getElementById("menu-icon");
+    const navMenu = document.getElementById("nav-menu");
+
+    menuIcon.addEventListener("click", () => {
+        navMenu.classList.toggle("open");
+    });
+
+    // Close menu when a link is clicked (for better UX)
+    document
+        .querySelectorAll(".desktop-nav__items-list__link")
+        .forEach((link) => {
+            link.addEventListener("click", () => {
+                navMenu.classList.remove("open");
+            });
+        });
+});
